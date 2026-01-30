80c97784d0992dd7b3d8773d997950befdb9379e
Cahit Ugur <cht.ugur@gmail.com>
2026-01-30 23:36:28 +0000

    Add new usual suspect player

--- shared-data.js
+++ shared-data.js
@@ -4,4 +4,4 @@
   Shared data for Poker Calculator apps
 */
 
-const USUAL_SUSPECTS = ['Brian', 'Cahit', 'Colm', 'Cormac', 'Dillon', 'Euan', 'Evangelia', 'Ferhat', 'Hannan', 'James', 'Kevin', 'Kunal', 'Liam', 'Marcus', 'Mic', 'Muireann', 'Otto', 'Patryk', 'Pawel', 'Scott', 'Tuhin'];
+const USUAL_SUSPECTS = ['Brian', 'Cahit', 'Colm', 'Cormac', 'Dillon', 'Euan', 'Evangelia', 'Ferhat', 'Hannan', 'James', 'Kevin', 'Kunal', 'Liam', 'Luke', 'Marcus', 'Mic', 'Muireann', 'Otto', 'Patryk', 'Pawel', 'Scott', 'Tuhin'];
