9585d28f4ce582e6d9fd4840723ec29c3cf83678
Cahit Ugur <cht.ugur@gmail.com>
2026-02-10 20:42:23 +0000

    add legacy to docs

---
+++ docs/shared-data.js
@@ -0,0 +1,7 @@
+/*
+  Copyright (c) 2025 Cahit Ugur
+  SPDX-License-Identifier: MIT
+  Shared data for Poker Calculator apps
+*/
+
+const USUAL_SUSPECTS = ['Brian', 'Cahit', 'Colm', 'Cormac', 'Dillon', 'Euan', 'Evangelia', 'Ferhat', 'Hannan', 'James', 'Kevin', 'Kunal', 'Liam', 'Luke', 'Marcus', 'Mic', 'Muireann', 'Otto', 'Patryk', 'Pawel', 'Scott', 'Tuhin'];
